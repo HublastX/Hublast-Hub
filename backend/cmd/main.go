2d7976bc215615280ae05e94fe7586e8a245eace
wendellast <wendelalves01a@hotmail.com>
2025-05-04 17:36:30 -0300

    docs(main): adiciona comentários swagger para documentação api

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -4,10 +4,27 @@ import (
 	"github.com/HublastX/HubLast-Hub/internal/api/routes"
 	"github.com/HublastX/HubLast-Hub/pkg/database"
 	"github.com/gin-gonic/gin"
+
+	_ "github.com/HublastX/HubLast-Hub/docs"
 )
 
-func main() {
+// @title           HubLast-Hub API
+// @version         1.0
+// @description     Project Management System API
 
+// @contact.name   API Support
+// @contact.email  support@hublast.com
+
+// @license.name  Apache 2.0
+// @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host      localhost:3005
+// @BasePath  /api
+
+// @securityDefinitions.apikey BearerAuth
+// @in header
+// @name Authorization
+func main() {
 	database.Connect()
 
 	router := gin.Default()
