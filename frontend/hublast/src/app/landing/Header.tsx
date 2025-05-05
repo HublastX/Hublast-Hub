10415cb5b4cd29a96fceb7f62b25f543f7083a36
Andra-sun <camileguimaraes463@gmail.com>
2025-05-05 11:02:00 -0300

    feat(header): header da landing ajustado para o mobile

--- frontend/hublast/src/app/landing/Header.tsx
+++ frontend/hublast/src/app/landing/Header.tsx
@@ -8,8 +8,8 @@ export default function Header() {
     ];
 
   return (
-    <header className="fixed w-full py-4 px-6 flex justify-between items-cente ">
-      <div className="flex items-center  gap-4">
+    <header className="fixed w-full py-4 px-6 flex justify-between items-cente backdrop-blur-md md:backdrop-blur-none">
+      <div className="flex items-center gap-4">
         <Image
           src="https://avatars.githubusercontent.com/u/155011581?s=200&v=4"
           alt="Ícone da Hublast"
@@ -17,10 +17,10 @@ export default function Header() {
           height={60}
           className="rounded-full"
         />
-        <h1 className="text-2xl rounded pr-3 py-2 font-extrabold backdrop-blur-md">Hublast</h1>
+        <h1 className="text-2xl rounded pr-3 py-2 font-extrabold md:backdrop-blur-md">Hublast</h1>
       </div>
 
-      <nav className="flex gap-6 pl-3 py-2 rounded backdrop-blur-md h-fit">
+      <nav className="flex gap-6 pl-3 py-2 rounded md:backdrop-blur-md md:h-fit items-center">
         {listHeader.map((item, index) => (
           <Link
             key={index}
