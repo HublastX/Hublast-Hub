237099939b18090b7a8952c72389f9045122bb16
Andra-sun <camileguimaraes463@gmail.com>
2025-05-05 15:35:38 -0300

    style(logo): pasta style subistituida por assets

--- frontend/hublast/src/app/assets/Logo.tsx
+++ frontend/hublast/src/app/assets/Logo.tsx
@@ -12,7 +12,7 @@ export default function Logo({ className }: LogoProps) {
             <ellipse
                 style={{
                     stroke: "currentColor",
-                    strokeWidth: 15,
+                    strokeWidth: 30,
                     fill: "none",
                 }}
                 cx="353.616"
@@ -27,7 +27,7 @@ export default function Logo({ className }: LogoProps) {
                     fill: "none",
                     strokeLinecap: "round",
                     strokeLinejoin: "round",
-                    strokeWidth: 12,
+                    strokeWidth: 20,
                 }}
                 d="M 343.381 166.334 C 342.759 159.565 345.008 61.63 345.393 61.47 C 365.562 53.133 393.666 171.534 365.078 357.11 C 336.49 542.686 377.951 695.226 383.709 695.348 C 388.823 695.457 391.297 592.898 390.561 578.536"
             ></path>
